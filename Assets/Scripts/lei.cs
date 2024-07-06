47d101b33b201fc62959f64496d46032e4db483f
雷昌政 <3375599273@qq.com>
2024-07-06 23:44:35 +0800

    测试提交

---
+++ Assets/Scripts/lei.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lei : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("第一次提交到github上");   
+    }
+
+
+}
